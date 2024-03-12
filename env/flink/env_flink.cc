61f9574773fbfdae7b2f71bd8f861605afead3ec
yhx <38719192+masteryhx@users.noreply.github.com>
2024-03-12 11:25:14 +0800

    [env] Introduce interface of env_flink (#5)

---
+++ env/flink/env_flink.cc
@@ -0,0 +1,10 @@
+//  Copyright (c) 2021-present, Facebook, Inc.  All rights reserved.
+//  This source code is licensed under both the GPLv2 (found in the
+//  COPYING file in the root directory) and Apache 2.0 License
+//  (found in the LICENSE.Apache file in the root directory).
+
+// TODO:
+//  1. Register flink env to ObjectLibrary
+//  2. Implement all methods of env_flink.h
+
+#include "env_flink.h"
