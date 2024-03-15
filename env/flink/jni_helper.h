f845fe48eee62bef6b3abb76127e15c87466cfd2
Hangxiang Yu <masteryhx@gmail.com>
2024-03-15 09:38:58 +0800

    [env] Implement all methods of env_flink (#13)
    (cherry picked from commit 7c0c8da59ae8d27b7db68752ac84ec3004efba87)

--- env/flink/jni_helper.h
+++ env/flink/jni_helper.h
@@ -3,8 +3,11 @@
 //  COPYING file in the root directory) and Apache 2.0 License
 //  (found in the LICENSE.Apache file in the root directory).
 
+#include <functional>
+#include <vector>
+
 #include "jni.h"
-#include "rocksdb/status.h"
+#include "rocksdb/io_status.h"
 
 namespace ROCKSDB_NAMESPACE {
 
@@ -24,22 +27,98 @@ class JavaClassCache {
     NUM_CACHED_CLASSES
   } CachedJavaClass;
 
-  // Constructor and Destructor
-  explicit JavaClassCache(JNIEnv* env);
-  ~JavaClassCache();
+  // Frequently-used method type representing jmethods which will be cached.
+  typedef enum {
+    JM_FLINK_PATH_CONSTRUCTOR,
+    JM_FLINK_PATH_TO_STRING,
+    JM_FLINK_URI_CONSTRUCTOR,
+    JM_FLINK_FILE_SYSTEM_GET,
+    JM_FLINK_FILE_SYSTEM_EXISTS,
+    JM_FLINK_FILE_SYSTEM_LIST_STATUS,
+    JM_FLINK_FILE_SYSTEM_GET_FILE_STATUS,
+    JM_FLINK_FILE_SYSTEM_DELETE,
+    JM_FLINK_FILE_SYSTEM_MKDIR,
+    JM_FLINK_FILE_SYSTEM_RENAME_FILE,
+    JM_FLINK_FILE_SYSTEM_OPEN,
+    JM_FLINK_FS_INPUT_STREAM_SEQ_READ,
+    JM_FLINK_FS_INPUT_STREAM_RANDOM_READ,
+    JM_FLINK_FS_INPUT_STREAM_SKIP,
+    JM_FLINK_FS_OUTPUT_STREAM_WRITE,
+    JM_FLINK_FS_OUTPUT_STREAM_FLUSH,
+    JM_FLINK_FS_OUTPUT_STREAM_SYNC,
+    JM_FLINK_FS_OUTPUT_STREAM_CLOSE,
+    JM_FLINK_FILE_SYSTEM_CREATE,
+    JM_FLINK_FILE_STATUS_GET_PATH,
+    JM_FLINK_FILE_STATUS_GET_LEN,
+    JM_FLINK_FILE_STATUS_GET_MODIFICATION_TIME,
+    JM_FLINK_FILE_STATUS_IS_DIR,
+    NUM_CACHED_METHODS
+  } CachedJavaMethod;
 
-  // Get jclass by specific CachedJavaClass
-  Status GetJClass(CachedJavaClass cachedJavaClass, jclass* javaClass);
-
- private:
-  typedef struct {
+  // jclass with its context description
+  struct JavaClassContext {
     jclass javaClass;
     const char* className;
-  } javaClassAndName;
 
+    std::string ToString() const {
+      return std::string("className: ").append(className);
+    }
+  };
+
+  // jmethod with its context description
+  struct JavaMethodContext {
+    JavaClassContext javaClassAndName;
+    jmethodID javaMethod;
+    const char* methodName;
+    const char* signature;
+
+    std::string ToString() const {
+      return javaClassAndName.ToString()
+          .append(", methodName: ")
+          .append(methodName)
+          .append(", signature: ")
+          .append(signature);
+    }
+  };
+
+  ~JavaClassCache();
+
+  // Create a unique instance which inits necessary cached classes and methods.
+  // Return Status representing whether these classes and methods are inited
+  // correctly or not.
+  static IOStatus Create(JNIEnv* env,
+                         std::unique_ptr<JavaClassCache>* javaClassCache);
+
+  // Get JavaClassContext by specific CachedJavaClass.
+  JavaClassContext GetJClass(CachedJavaClass cachedJavaClass);
+
+  // Get JavaMethodContext by specific CachedJavaMethod.
+  JavaMethodContext GetJMethod(CachedJavaMethod cachedJavaMethod);
+
+  // Construct Java Path Instance based on cached classes and method related to
+  // Path.
+  IOStatus ConstructPathInstance(const std::string& /*file_path*/,
+                                 jobject* /*pathInstance*/);
+
+ private:
   JNIEnv* jni_env_;
-  javaClassAndName cached_java_classes_[JavaClassCache::NUM_CACHED_CLASSES];
+  JavaClassContext cached_java_classes_[CachedJavaClass::NUM_CACHED_CLASSES];
+  JavaMethodContext cached_java_methods_[CachedJavaMethod::NUM_CACHED_METHODS];
 
-  Status initCachedClass(const char* className, jclass* cachedClass);
+  explicit JavaClassCache(JNIEnv* env);
+
+  // Init all classes and methods.
+  IOStatus Init();
+
+  // Init cached class.
+  IOStatus initCachedClass(const char* className, jclass* cachedClass);
 };
+
+// Return current status of JNIEnv.
+IOStatus CurrentStatus(
+    const std::function<std::string()>& /*exceptionMessageIfError*/);
+
+// Wrap error status of JNIEnv.
+IOStatus CheckThenError(const std::string& /*exceptionMessageIfError*/);
+
 }  // namespace ROCKSDB_NAMESPACE
